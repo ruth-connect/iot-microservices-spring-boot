a1623c8b13bc364181624511252d68dd3831f1dd
Ruth Mills <ruth@megtek.co.uk>
2016-01-12 21:54:13 +0000

    Initial creation.

---
+++ src/main/java/uk/me/ruthmills/iot/IotMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package uk.me.ruthmills.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotMicroservicesApplication.class, args);
+	}
+}
---
+++ src/main/java/uk/me/ruthmills/iot/controller/EasyBulbController.java
@@ -0,0 +1,90 @@
+package uk.me.ruthmills.iot.controller;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import de.toman.milight.MiLightWiFiBox;
+
+@Controller
+@RequestMapping("/easybulb")
+public class EasyBulbController {
+	
+	private MiLightWiFiBox easyBulb;
+	
+	public EasyBulbController() throws UnknownHostException {
+		easyBulb = new MiLightWiFiBox("easybulb");
+	}
+
+	@RequestMapping("/off")
+	@ResponseStatus(value = HttpStatus.OK)
+	public void off() throws IOException {
+		easyBulb.off();
+	}
+	
+	@RequestMapping("/off/{group}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public void off(@PathVariable Integer group) throws IOException {
+		easyBulb.off(group);
+	}
+	
+	@RequestMapping("/on")
+	@ResponseStatus(value = HttpStatus.OK)
+	public void on() throws IOException {
+		easyBulb.on();
+	}
+	
+	@RequestMapping(value = "/on/{group}", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void on(@PathVariable Integer group) throws IOException {
+		easyBulb.on(group);
+	}
+	
+	@RequestMapping(value = "/white", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void white() throws IOException {
+		easyBulb.white();
+	}
+	
+	@RequestMapping(value = "/white/{group}", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void white(@PathVariable Integer group) throws IOException {
+		easyBulb.white(group);
+	}
+	
+	@RequestMapping(value = "/discoMode/{group}", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void discoMode(@PathVariable Integer group) throws IOException {
+		easyBulb.discoMode(group);
+	}
+	
+	@RequestMapping(value = "/discoModeFaster", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void discoModeFaster() throws IOException {
+		easyBulb.discoModeFaster();
+	}
+	
+	@RequestMapping(value = "/discoModeSlower", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void discoModeSlower() throws IOException {
+		easyBulb.discoModeSlower();
+	}
+	
+	@RequestMapping(value = "/brightness/{group}/{value}", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void brightness(@PathVariable Integer group, @PathVariable Integer value) throws IOException {
+		easyBulb.brightness(group, value);
+	}
+	
+	@RequestMapping(value = "/colour/{group}/{value}", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void colour(@PathVariable Integer group, @PathVariable Integer value) throws IOException {
+		easyBulb.color(group, value);
+	}
+}
